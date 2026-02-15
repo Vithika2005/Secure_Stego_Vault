7fca81329f9a022feaa1fbef924e937b17f4911d
Vithika Surve <136482308+Vithika2005@users.noreply.github.com>
2026-02-15 10:56:23 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+
+# config.py
+
+VAULT_FILE = "data/vault.enc"
+STEGO_IMAGE = "data/vault.png"
+CARRIER_IMAGE = "assets/sample.png"
+
+ARGON2_TIME_COST = 2
+ARGON2_MEMORY_COST = 102400
+ARGON2_PARALLELISM = 8
+KEY_LENGTH = 32
+
+
